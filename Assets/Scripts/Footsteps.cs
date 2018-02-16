47df2ab66b21855b00fc1ac6e127c752e34e7f54
daivscott <daivgit@mail.com>
2018-02-16 05:22:54 +0000

    First Steps
    Footstep sounds added to run animation.

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour {
+
+    public AudioClip[] leftClips;
+    public AudioClip[] rightClips;
+
+    private AudioSource audioSource;
+    private bool leftFoot;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Step()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        if (leftFoot)
+        {
+            leftFoot = !leftFoot;
+            return leftClips[UnityEngine.Random.Range(0, leftClips.Length)];            
+        }
+        else
+        {
+            leftFoot = !leftFoot;
+            return rightClips[UnityEngine.Random.Range(0, rightClips.Length)];
+        }
+    }
+}
